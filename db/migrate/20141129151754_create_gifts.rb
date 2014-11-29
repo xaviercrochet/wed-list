4c22c6724820392e6f5044a2152ab2cdce4d3c07
Xavier Crochet <xacrochet@gmail.com>
2014-11-29 16:28:13 +0100

    Fixed gift migration

--- db/migrate/20141129151754_create_gifts.rb
+++ db/migrate/20141129151754_create_gifts.rb
@@ -2,10 +2,9 @@ class CreateGifts < ActiveRecord::Migration
   def change
     create_table :gifts do |t|
       t.string :title
-      t.boolean :status
-      t.true :default
+      t.boolean :status, default: true
       t.integer :price
-      t.availability :availability
+      t.integer :availability
 
       t.timestamps
     end
