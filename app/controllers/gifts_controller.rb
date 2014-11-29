00190eaffc2dce166bb9fe4b1ae8bfab2954c70c
Xavier Crochet <xacrochet@gmail.com>
2014-11-29 17:11:22 +0100

    Added gift creationw form & coresponding controller action

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,4 +1,22 @@
 class GiftsController < ApplicationController
   def index
   end
+
+  def new
+    @gift = Gift.new
+  end
+
+  def create
+    @gift = Gift.create(gift_params)
+    redirect_to @gift
+  end
+
+  def show
+  end
+
+  private
+
+    def gift_params
+      params.require(:gift).permit(:title, :price, :availability)
+    end
 end
