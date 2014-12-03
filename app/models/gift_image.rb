6f1bfdabdede238ad89444bef1a5aa7b978bda6d
Xavier Crochet <xacrochet@gmail.com>
2014-12-03 21:34:38 +0100

    Photo can now be multi-added through js

--- app/models/gift_image.rb
+++ app/models/gift_image.rb
@@ -1,7 +1,7 @@
 class GiftImage < ActiveRecord::Base
 
   belongs_to :gift
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :medium => "300x300>"}
 
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
