d7c22d8f00a189d9c8236d4f061449fc3c4acd5d
Xavier Crochet <xacrochet@gmail.com>
2014-12-10 21:58:37 +0100

    Devise ftw o/

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,33 +1,34 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_sign_up_params, only: [:create]
   before_filter :configure_account_update_params, only: [:update]
+  before_filter :update_sanitized_params, if: :devise_controller?
 
-  GET /resource/sign_up
+
   def new
    super
   end
 
-  POST /resource
+
   def create
    super
   end
 
-  GET /resource/edit
+
   def edit
    super
   end
 
-  PUT /resource
+
   def update
    super
   end
 
-  DELETE /resource
+
   def destroy
    super
   end
 
-  GET /resource/cancel
+
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
   # cancel oauth signing in/up in the middle of the process,
@@ -56,5 +57,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # The path used after sign up for inactive accounts.
   def after_inactive_sign_up_path_for(resource)
    super(resource)
+  end
+
+  def update_sanitized_params
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation)}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {sessions: "users/sessions"}
-  resources :users
+  devise_for :users, controllers: {registrations: "users/registrations"}
 
   resources :gifts
 
