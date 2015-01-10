01d3f6d26287f2cc877a08e65891cea9f9b1f3ae
Xavier Crochet <xacrochet@gmail.com>
2015-01-10 17:35:53 +0100

    Finished creation form

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   has_many :transactions, dependent: :destroy
   has_many :orders, dependent: :destroy
+  has_many :songs, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
