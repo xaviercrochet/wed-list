519d2b0fafba6e3869532912ce9301e04265f0f6
Xavier Crochet <xacrochet@gmail.com>
2015-01-10 18:00:22 +0100

    Youtube search url is now default url for songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :user
+
+  after_create :generate_youtube_url_if_nil_url
+
+  def generate_youtube_url_if_nil_url
+    if self.url.empty?
+      self.url = "http://www.youtube.com/results?search_query="+self.artist+"+"+self.title
+      self.save
+    end
+  end
 end
