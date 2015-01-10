8100327fd049cfa24d581fca008a8ba654dfc263
Xavier Crochet <xacrochet@gmail.com>
2015-01-10 19:08:21 +0100

    Updated song.url validation

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :user
-  validates :url, :format => URI::regexp(%w(http https))
+  validates :url, :format => URI::regexp(%w(http https)), :if => :not_empty?
   validates_presence_of :title, :artist
   after_create :generate_youtube_url_if_nil_url
 
@@ -9,6 +9,10 @@ class Song < ActiveRecord::Base
       self.url = "http://www.youtube.com/results?search_query="+self.artist+"+"+self.title
       self.save
     end
+  end
+
+  def not_empty?
+    ! self.url.empty?
   end
 
 
