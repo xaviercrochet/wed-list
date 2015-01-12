1cb4685d864810db6b179c2684e3688d28d5dba0
Xavier Crochet <xacrochet@gmail.com>
2015-01-12 17:39:21 +0100

    Modified user controller regarding new attribute

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :coming, :phone)
   end
 end
