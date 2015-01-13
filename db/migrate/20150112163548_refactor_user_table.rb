ea053e3b5c08e41fcacd76ab53bb3fdb65fdef0b
Xavier Crochet <xacrochet@gmail.com>
2015-01-13 11:57:42 +0100

    Added index in user migration

--- db/migrate/20150112163548_refactor_user_table.rb
+++ db/migrate/20150112163548_refactor_user_table.rb
@@ -1,6 +1,6 @@
 class RefactorUserTable < ActiveRecord::Migration
   def change
-    add_column :users, :phone, :string
+    add_column :users, :phone, :string, index: true
     add_column :users, :coming, :boolean, default: false
   end
 end
