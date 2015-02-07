d43655abf1e1a5c3f6b5adfe13608ceaafb8da54
Xavier Crochet <xacrochet@gmail.com>
2015-02-07 18:57:33 +0100

    Added participation validations

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,3 +1,26 @@
 class Participation < ActiveRecord::Base
   belongs_to :user
+
+  validate :adults_or_kids_must_be_strictly_positive
+  validate :adults_must_be_positive
+  validate :kids_must_be_positive
+
+  def adults_or_kids_must_be_strictly_positive
+    if ! (adults > 0 or kids > 0)
+      errors.add(:adults, "Au moins une personne doit être présente")
+      errors.add(:kids, "Au moins une personne doit être présente")
+    end
+  end
+
+  def adults_must_be_positive
+    if adults < 0
+      errors.add(:adults, "Doit être >= 0")
+    end
+  end
+
+  def kids_must_be_positive
+    if kids < 0
+      errors.add(:kids, "Doit être >= 0")
+    end
+  end
 end
