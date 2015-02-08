abf740d20dc5d3714b04a92fd1f4acd6cc875ba3
Xavier Crochet <xacrochet@gmail.com>
2015-02-08 12:19:54 +0100

    Added participation success & errror message

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -7,12 +7,33 @@ class ParticipationsController < ApplicationController
   end
 
   def create
+    @participation = current_user.create_participation(participation_params)
+    if ! @participations.errors.any?
+      flash[:success] = true
+    end
+    redirect_to edit_user_participation_path(current_user, @participation)
+
   end
 
   def destroy
+    current_user.participation.destroy
+    redirect_to new_user_participation_path(current_user)
+  end
+
+  def update
+    @participation = current_user.participation
+    @participation.update(participation_params)
+    if ! @participation.errors.any?
+      flash[:success] = true
+    end
+    redirect_to edit_user_participation_path(current_user, @participation)
   end
 
   def new
     @participation = Participation.new
+  end
+private
+  def participation_params
+    params.require(:participation).permit(:adults, :kids)
   end
 end
